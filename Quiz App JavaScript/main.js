040b8d5b813e19248e6e995c1906da9436420585
Satish kushwaha <148550819+Satishkush4446@users.noreply.github.com>
2024-01-06 05:24:33 +0000

    update-quiz

---
+++ Quiz App JavaScript/main.js
@@ -0,0 +1,50 @@
+const questions = [
+    {
+      'que': 'Which of the following is a markup language?',
+      'a': 'HTML',
+      'b': 'CSS',
+      'c': 'JavaScript',
+      'd': 'PHP',
+      'correct': 'a'
+    },
+    {
+      'que': 'What is the capital of India?',
+      'a': 'Beijing',
+      'b': 'Seoul',
+      'c': 'New Delhi',
+      'd': 'Bangkok',
+      'correct': 'c'
+    },
+    {
+      'que': 'Which planet is known as the Red Planet?',
+      'a': 'Mars',
+      'b': 'Venus',
+      'c': 'Jupiter',
+      'd': 'Saturn',
+      'correct': 'a'
+    },
+    {
+      'que': 'Which programming language is used for building web applications?',
+      'a': 'Java',
+      'b': 'Python',
+      'c': 'Ruby',
+      'd': 'JavaScript',
+      'correct': 'd'
+    },
+    {
+      'que': 'Which of the following is a front-end framework/library?',
+      'a': 'Node.js',
+      'b': 'React',
+      'c': 'Express',
+      'd': 'Django',
+      'correct': 'b'
+    }
+  ];
+
+
+let index = 0 ;
+const quesBox = 
+const loadQuestion = () =>{
+        const data = questions[index]
+}
+loadQuestion();
